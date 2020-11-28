fdbbfc89127337cf5073f1787493f1543e8ba425
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-11-28 03:20:11 -0800

    Add boilerplate OnRenderImage code to debug script

--- Assets/DebugMaps.cs
+++ Assets/DebugMaps.cs
@@ -3,5 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DebugMaps : MonoBehaviour {
+    public ComputeShader mapGenerator;
 
+    private RenderTexture target;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        if (target == null) {
+            target = new RenderTexture(256, 256, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            target.enableRandomWrite = true;
+            target.Create();
+        }
+
+        Graphics.Blit(target, destination);
+    }
 }
