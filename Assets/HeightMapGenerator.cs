6db714773bf72b1046e36c9b0c72ebf6638d30db
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-05-02 04:02:48 -0700

    Generate map with height map generator

--- Assets/HeightMapGenerator.cs
+++ Assets/HeightMapGenerator.cs
@@ -4,4 +4,26 @@ using UnityEngine;
 
 public class HeightMapGenerator {
 
+    private RenderTexture map;
+    private ComputeShader computeMap;
+
+    public HeightMapGenerator(int width, int height) {
+        if (map == null) {
+            map = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            map.enableRandomWrite = true;
+            map.Create();
+        }
+
+        computeMap = (ComputeShader)Resources.Load("MapGenerator");
+        computeMap.SetTexture(0, "_Result", map);
+    }
+
+    public RenderTexture GenerateMap(uint seed) {
+        computeMap.SetInt("_Seed", (int)seed);
+        int threadGroupsX = Mathf.CeilToInt(map.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(map.height / 8.0f);
+        computeMap.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+        return map;
+    }
 }
